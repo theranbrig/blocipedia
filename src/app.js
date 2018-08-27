4fb9ba61d84009eabd09aa2228242cb38990bf23
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-08-28 01:56:26 +0900

    added controller modules

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const app = express();
 const appConfig = require('./config/main-config.js');
-
-app.use('/', (req, res, next) => {
-	res.send('Welcome to Bloccit');
-});
-
 const routeConfig = require('./config/route-config.js');
 
 routeConfig.init(app);
-appConfig.init()
+appConfig.init(app, express);
 
 module.exports = app;
