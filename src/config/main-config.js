a57906885ae1f4dfcc74c3df0a2ef23f399514da
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-08-29 02:36:02 +0900

    added PureCSS and basic structure

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -5,7 +5,7 @@ const viewsFolder = path.join(__dirname, '..', 'views');
 module.exports = {
 	init(app, express) {
 		app.set('views', viewsFolder);
-		app.set('view engine', 'pug');
+		app.set('view engine', 'ejs');
 		app.use(express.static(path.join(__dirname, '..', 'assets')));
 	}
 };
