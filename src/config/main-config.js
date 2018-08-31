ad1e01f37337c110b569a8641634cf0b54ccb78a
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-08-31 13:15:40 +0900

    fixed send grid styling

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -12,6 +12,7 @@ module.exports = {
 		app.set('views', viewsFolder);
 		app.set('view engine', 'ejs');
 		app.use(bodyParser.urlencoded({ extended: true }));
+		app.use(express.static(path.join(__dirname, '..', 'assets')));
 		app.use(expressValidator());
 		app.use(
 			session({
@@ -27,6 +28,5 @@ module.exports = {
 			res.locals.currentUser = req.user;
 			next();
 		});
-		app.use(express.static(path.join(__dirname, '..', 'assets')));
 	}
 };
