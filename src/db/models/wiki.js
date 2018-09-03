e3bdb37bdbb8c41a8b05cd3c29d29f4229f8f61e
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-09-04 01:13:44 +0900

    WIP wiki CRUD basics created

---
+++ src/db/models/wiki.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+	const Wiki = sequelize.define(
+		'Wiki',
+		{
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			body: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			private: {
+				type: DataTypes.BOOLEAN,
+				allowNull: false,
+				defaultValue: false
+			},
+			userId: {
+				type: DataTypes.INTEGER,
+				onDelete: 'CASCADE',
+				references: {
+					model: 'Users',
+					key: 'id',
+					as: 'userId'
+				}
+			}
+		},
+		{}
+	);
+	Wiki.associate = function(models) {
+		Wiki.belongsTo(models.User, {
+			foreignKey: 'userId',
+			onDelete: 'CASCADE'
+		});
+	};
+	return Wiki;
+};
--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,4 +1,22 @@
 module.exports = {
+	validateWikis(req, res, next) {
+		if (req.method === 'POST') {
+			req
+				.checkParams('id', 'must be valid')
+				.notEmpty()
+				.isInt();
+			req.checkBody('title', 'Must be between 2 and 15 characters').isLength({ min: 2, max: 15 });
+			req.checkBody('body', 'Must be between 5 and 20 characters ').isLength({ min: 5, max: 20 });
+		}
+		const errors = req.validationErrors();
+
+		if (errors) {
+			req.flash('error', errors);
+			return res.redirect(303, req.headers.referer);
+		} else {
+			return next();
+		}
+	},
 	validateUsers(req, res, next) {
 		if (req.method === 'POST') {
 			req.checkBody('email', 'must be valid').isEmail();
