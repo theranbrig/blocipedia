25d34fbfdbf5feadf055cc228039ad0c578daf8a
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-09-09 19:08:05 +0900

    added fast facts and paragraphs to seeds

--- src/db/seeds/20180905144312-wiki.js
+++ src/db/seeds/20180905144312-wiki.js
@@ -5,12 +5,13 @@ const faker = require('faker');
 let wikis = [];
 for (let i = 1; i < 15; i++) {
 	wikis.push({
-		title: `${faker.commerce.productMaterial()} ${faker.commerce.product()}`,
-		body: `${faker.commerce.productName()}`,
-		image1: 'https://picsum.photos/800/1200/?random',
-    image2: 'https://picsum.photos/400/600/?random',
-    userId: faker.random.number({ min: 1, max: 10 }),
-    private: false,
+		title: `${faker.commerce.productName()}`,
+		body: faker.lorem.paragraph(),
+		image1: 'https://picsum.photos/500/1100/?random',
+		image2: 'https://picsum.photos/400/600/?random',
+		userId: faker.random.number({ min: 1, max: 10 }),
+		fastFacts: faker.lorem.paragraph(),
+		private: false,
 		createdAt: new Date(),
 		updatedAt: new Date()
 	});
